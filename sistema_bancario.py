e11ed597c2616c1fbeb1dc70e72fd194a541dec0
Matheus Vincius Teixeira <148890495+MatheusVTP@users.noreply.github.com>
2025-06-30 14:51:33 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -18,30 +18,30 @@ while True:
     opcao = input(menu)
 
     if opcao == "0":
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Insira o valor do depósito: "))
 
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Operação falha! O valor informado é inválido.")
 
     elif opcao == "1":
-        valor = float(input("Informe o valor do saque: "))
+        valor = float(input("Insira o valor do saque: "))
 
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > limite
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Operação invalida! Você não tem saldo suficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("Operação invalida! O valor do saque excede o limite.")
 
         elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("Operação invalida! Número máximo de saques excedido.")
 
         elif valor > 0:
             saldo -= valor
